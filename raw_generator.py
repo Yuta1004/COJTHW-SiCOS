c5402930396899cd4555758d525d8dd3892d16c5
NakagamiYuta <guguru0014@gmail.com>
2022-12-16 06:44:11 +0900

    [add] raw_generator.py

---
+++ raw_generator.py
@@ -0,0 +1,24 @@
+import sys
+
+from elftools.elf.elffile import ELFFile
+
+
+def write_raw(out, body):
+    with open(out, "wb") as f:
+        f.write(body)
+
+
+def parse(out, f):
+    elffile = ELFFile(f)
+    for segment in elffile.iter_segments():
+        if segment.header["p_paddr"] == 0x20000000:
+            write_raw(out, segment.data())
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python3 raw_generator.py <OUT> <ELF>")
+        exit(0)
+
+    with open(sys.argv[2], "rb") as f:
+        parse(sys.argv[1], f)
