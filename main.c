337260c1887522155a2c580ce2b97c19d3b97040
NakagamiYuta <guguru0014@gmail.com>
2022-12-16 20:17:57 +0900

    [add] シリアル通信できることを確認

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
+void println(char *s) {
+    for (char *p = s; *p != '\0'; ++ p) {
+        while(*((volatile unsigned int*)0x80010008) != 4) {}
+        *((volatile unsigned int*)0x80010004) = *p;
+    }
+    *((volatile unsigned int*)0x80010004) = 0x0d;
+    *((volatile unsigned int*)0x80010004) = 0x0a;
+}
+
 int main() {
-    int a = 10;
-    int b = 20;
-    return a + b;
+    *((volatile unsigned int*)0x8001000C) = 0b00011;
+
+    println("Hello World!! From C Lang Program!!!\0");
+
+    return 0;
 }
