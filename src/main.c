c835e42baa7a52ced3bef3db60a9b290f90d429a
NakagamiYuta <guguru0014@gmail.com>
2022-12-21 14:42:51 +0900

    [add] フィボナッチ数列の計算→出力ができることを確認

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include "uart.h"
+#include "muldiv.h"
+
+int fib(int n) {
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    return fib(n - 1) + fib(n - 2);
+}
 
 int main() {
     uart_init();
-    uart_printsln("Hello! I'm COJT-original RISC-V(RV32I) core!");
-    return 0;
+
+    uart_printsln("Hello, I'm COJT-original RISC-V(RV32I) core!");
+
+    for (int n = 1; n <= 15; ++ n) {
+        uart_prints("fib(");
+        uart_printd(n);
+        uart_prints(") = ");
+        uart_printdln(fib(n));
+    }
 }
