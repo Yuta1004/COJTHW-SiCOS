05b62306a1e223bfe22202948f9fcb89d49dccfa
NakagamiYuta <guguru0014@gmail.com>
2022-12-23 14:12:17 +0900

    [add](dev/cojt) init_xclk移植

---
+++ src/dev/cojt/init_xclk.c
@@ -0,0 +1,11 @@
+#include "init_dvi.h"
+#include "init_xclk.h"
+
+#define CDCE925_BYTE_RW 0x80
+
+void init_xclk(unsigned char *XCLK) {
+    iic_switch(IIC_SEL_DVI);
+    for (int i = 0; i < 48; ++ i) {
+        iic_write(0xC8, CDCE925_BYTE_RW | i, XCLK[i]);
+    }
+}
