4e264c6284a01ee8c468c69874995b4520cc8d5a
NakagamiYuta <guguru0014@gmail.com>
2022-12-24 02:49:10 +0900

    [add] シェルもどき作成

--- src/main.c
+++ src/main.c
@@ -9,47 +9,31 @@
 #include "dev/cojt/xclk.h"
 
 int main() {
-    /* 周辺機器初期設定 */
+    // 周辺機器初期設定
     init_tpf410();
     init_xclk(XCLK24MHz);
     init_ov9655(OV9655_SXGA);
 
-    /* 回路初期化 */
+    // 回路初期化
     set_resolution(SXGA);
     uart_init();
 
-    display_on(0x3fa00000);
+    // 起動メッセージ
+    uart_printsln("+--------------+");
+    uart_printsln("| SiCOS v0.0.1 |");
+    uart_printsln("+--------------+");
+    uart_printsln("");
 
-    draw_set_frame(0x3fa00000, 1280, 1024);
-    draw_set_drawarea(0, 0, 1280, 1024);
-
-    draw_set_color(0, 0, 255, 0);
-    draw_box(0, 0, 1280, 1024);
-
-    int ox = 0; int oy = 0;
-    int px = 0; int py = 0;
-    int vx = 5; int vy = 5;
+    // シェル
     while (1) {
-        draw_set_frame(0x3fa00000, 1280, 1024);
-        draw_set_drawarea(0, 0, 1280, 1024);
+        // プロンプト
+        uart_prints("> ");
 
-        draw_set_color(0, 0, 255, 0);
-        draw_box(ox, oy, 100, 100);
+        // ユーザ入力待機
+        char *cmd = uart_inputs();
 
-        draw_set_color(0, 255, 0, 0);
-        draw_box(px, py, 100, 100);
-
-        draw_exec();
-
-        ox = px; oy = py;
-        px += vx; py += vy;
-        if (px < 0 || px > 1280-100) {
-            vx *= -1;
-        }
-        if (py < 0 || py > 1024-100) {
-            vy *= -1;
-        }
-
-        for (volatile int cnt = 0; cnt < 5000; ++ cnt);
+        uart_prints("Exec => \"");
+        uart_prints(cmd);
+        uart_printsln("\"\n");
     }
 }
