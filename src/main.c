b6f1522fa3f5786ffcf5bcec83c03f3cda42b8d2
NakagamiYuta <guguru0014@gmail.com>
2022-12-24 04:14:16 +0900

    [add] コマンド実行できるようにした

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "calc/muldiv.h"
+#include "calc/string.h"
 #include "dev/uart.h"
 #include "dev/display.h"
 #include "dev/camera.h"
@@ -7,6 +8,27 @@
 #include "dev/cojt/dvi.h"
 #include "dev/cojt/ov9655.h"
 #include "dev/cojt/xclk.h"
+
+int test(int argc, char **argv) {
+    uart_prints("argc : ");
+    uart_printd(argc);
+    uart_prints("\nargv : ");
+    for (char **arg = argv; *arg != 0; ++ arg) {
+        uart_prints(*arg);
+        uart_prints(", ");
+    }
+    uart_printsln("");
+    return argc;
+}
+
+typedef struct {
+    char *name;
+    int (*fp)(int, char**);
+} cmd_info;
+
+cmd_info cmd_info_list[] = {
+    {"test", test}
+};
 
 int main() {
     // 周辺機器初期設定
@@ -25,19 +47,26 @@ int main() {
     uart_printsln("");
 
     // シェル
+    unsigned char stat = 0;
     while (1) {
+loop_top:
         // プロンプト
+        uart_printc('[');
+        uart_printd(stat);
+        uart_printc(']');
         uart_prints("> ");
 
         // ユーザ入力待機
         char *cmd = uart_inputs();
 
         // 引数分解
+        int argc = 0;
         char *argv[16];
         char **ap = argv;
         for (char *p = cmd; *p != '\0'; ) {
             while (*p == ' ') ++ p;
 
+            argc ++;
             *(ap++) = p;
             while (*p != ' ' && *p != '\0') ++ p;
 
@@ -45,5 +74,20 @@ int main() {
             *(p++) = '\0';
         }
         *ap = 0;
+
+        // コマンド実行
+        for (int idx = 0; idx < sizeof(cmd_info_list)/sizeof(cmd_info); ++ idx) {
+            char *name = cmd_info_list[idx].name;
+            if (strcmp(argv[0], name) == 0) {
+                stat = cmd_info_list[idx].fp(argc, argv);
+                uart_printsln("");
+                goto loop_top;
+            }
+        }
+
+        stat = 255;
+        uart_prints("Command \"");
+        uart_prints(argv[0]);
+        uart_printsln("\" is not found.\n");
     }
 }
