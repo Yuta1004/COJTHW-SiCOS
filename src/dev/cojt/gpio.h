8de1f5242dd5bfee1c8ebe4d167eb1c9473d5416
NakagamiYuta <guguru0014@gmail.com>
2022-12-23 14:24:53 +0900

    [add](dev/cojt) gpio.h

---
+++ src/dev/cojt/gpio.h
@@ -0,0 +1,12 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#define GPIO_BASEADDR   0x41200000
+#define RESOL_SET       (*(volatile unsigned int *)(GPIO_BASEADDR + 0x00))
+#define RESOL_CTRL      (*(volatile unsigned int *)(GPIO_BASEADDR + 0x04))
+
+#define VGA             0x00
+#define XGA             0x01
+#define SXGA            0x02
+
+#endif
