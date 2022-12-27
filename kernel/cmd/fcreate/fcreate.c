897ef5e6ab4c78d3e4474dd1eef45f359afc96b1
NakagamiYuta <guguru0014@gmail.com>
2022-12-28 01:19:28 +0900

    [update](cmd) fcreateコマンド実行時にサイズや権限を引数で指定できるようにした

--- kernel/cmd/fcreate/fcreate.c
+++ kernel/cmd/fcreate/fcreate.c
@@ -1,14 +1,17 @@
 #include "fcreate.h"
 #include "../../fs.h"
 #include "../../lib/uart.h"
+#include "../../lib/string.h"
 
 int cmd_fcreate(int argc, char **argv) {
-    if (argc < 2) {
-        uart_printsln("USAGE: fcreate <FILE_NAME>");
+    if (argc < 4) {
+        uart_printsln("USAGE: fcreate <FILE_NAME> <PERMISSION> <SIZE>");
     }
 
-    int result = f_create(argv[1], 4000);
-    if (result != 0) {
+    unsigned char permission = (unsigned char)strtol(argv[2], 0, 0);
+    unsigned int size = (unsigned int)strtol(argv[3], 0, 0);
+
+    if (f_create(argv[1], permission, size) != 0) {
         uart_printsln("Error!");
     }
 }
