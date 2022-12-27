3980379b2173a8dba339abf50d21c646caaafbbe
NakagamiYuta <guguru0014@gmail.com>
2022-12-28 07:03:17 +0900

    [add](apps) fibアプリケーション作成

---
+++ apps/fib/main.c
@@ -0,0 +1,25 @@
+#include <uart.h>
+#include <string.h>
+
+int fib(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fib(n-1) + fib(n-2);
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        uart_printsln("Usage: fib <N>");
+        return 1;
+    }
+
+    int n = (int)strtol(argv[1], 0, 0);
+    int result = fib(n);
+
+    uart_prints("fib(");
+    uart_printd(n);
+    uart_prints(") = ");
+    uart_printdln(result);
+
+    return 0;
+}
