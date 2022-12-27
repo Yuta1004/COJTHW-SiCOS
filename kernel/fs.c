d05d4fbcf1ffb156bd982d14f3213cb2d3d3a9c9
NakagamiYuta <guguru0014@gmail.com>
2022-12-27 21:32:29 +0900

    [add](fs) 登録済みファイルを走査するための関数を作成

--- kernel/fs.c
+++ kernel/fs.c
@@ -3,16 +3,30 @@
 void fs_init() {
     for (unsigned char *p = FS_SADDR; p < FS_EADDR; p += 4096) {
         FS_Entry *entry = (FS_Entry*)p;
-        entry->body = (unsigned char*)(entry) + 0x18;
-        if (entry->magic[0] == 0x12 && entry->magic[1] == 0x04 && entry->magic[2] == 0x41) {
-            p += (entry->size & (~0xfff));
+        entry->body = p + 0x18;
+        if (FUSED(entry)) {
+            entry += ((entry->size + 0x18) & (~0xfff));
         } else {
             entry->magic[0] = 0x12;
             entry->magic[1] = 0x04;
             entry->magic[2] = 0x41;
             entry->permission = 0;
+            entry->size = 0;
         }
     }
+}
+
+FS_Entry *fs_head() {
+    return (FS_Entry*)FS_SADDR;
+}
+
+FS_Entry *fs_next(FS_Entry *fp) {
+    unsigned char *p = (unsigned char*)fp;
+    p += ((fp->size + 0x18) & (~0xfff)) + 4096;
+    if (p < FS_EADDR) {
+        return (FS_Entry*)p;
+    }
+    return 0;
 }
 
 // int f_create(char *fname);
--- kernel/fs.h
+++ kernel/fs.h
@@ -1,10 +1,6 @@
 #ifndef FS_H
 #define FS_H
 
-#define FS_SIZE     (256 * 1024 * 1024)
-#define FS_SADDR    ((unsigned char*)0x25000000)
-#define FS_EADDR    (FS_SADDR + FS_SIZE)
-
 typedef struct {
     unsigned char magic[3];
     unsigned char permission;
@@ -13,7 +9,16 @@ typedef struct {
     unsigned char *body;
 } FS_Entry;
 
+#define FS_SIZE     (256 * 1024 * 1024)
+#define FS_SADDR    ((unsigned char*)0x25000000)
+#define FS_EADDR    (FS_SADDR + FS_SIZE)
+
+#define FCHECK(fp)  ((*((unsigned int*)(fp)) & 0xffffff) == 0x410412)
+#define FUSED(fp)   (FCHECK(fp) && (*((unsigned int*)(fp)) & 0xff000000) != 0)
+
 void fs_init();
+FS_Entry *fs_head();
+FS_Entry *fs_next(FS_Entry *fp);
 
 int f_create(char *fname);
 int f_remove(char *fname);
