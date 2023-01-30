2be193ddbcf69c88ccc6fdc62631af040e6c151c
NakagamiYuta <guguru0014@gmail.com>
2023-01-30 18:20:39 +0900

    [add](apps/window) ウィンドウ描画(雑)を行えるようにした

--- apps/window/main.c
+++ apps/window/main.c
@@ -2,6 +2,8 @@
 #include <mouse.h>
 #include <draw.h>
 
+#include "window.h"
+
 #define queue3_push(queue, x) {\
     (queue)[2] = (queue)[1];\
     (queue)[1] = (queue)[0];\
@@ -29,18 +31,33 @@ void draw_mouse(int x[3], int y[3], int z[3], int btn) {
         MOUSE_CLICKED(btn, MOUSE_RBTN) ? 255 : 0
     };
 
+    draw_set_stmode(0);
+
+    draw_set_color(0, 128, 128, 128);
+    draw_box(x[2], y[2], 10, 10);
+
+    draw_set_color(0, color[0], color[1], color[2]);
+    draw_box(x[0], y[0], 10, 10);
+}
+
+void draw_window(Window *w_list) {
+    for (Window *wp = w_list->next_p; wp != 0; wp = wp->next_p) {
+        draw_set_color(0, 200, 100, 100);
+        draw_box(wp->x0, wp->y0, wp->width, wp->height);
+    }
+}
+
+void draw(Window *w_list, int m_xpos[3], int m_ypos[3], int m_zpos[3], int m_btn) {
     DRAW_FRAME({
-        draw_set_stmode(0);
-
-        draw_set_color(0, 128, 128, 128);
-        draw_box(x[2], y[2], 10, 10);
-
-        draw_set_color(0, color[0], color[1], color[2]);
-        draw_box(x[0], y[0], 10, 10);
+        draw_window(w_list);
+        draw_mouse(m_xpos, m_ypos, m_zpos, m_btn);
     });
 }
 
 int main(int argc, char **argv) {
+    // ウィンドウ関連変数
+    Window *w_list = window_root();
+
     // マウス関連変数
     int tmp_m_xpos, tmp_m_ypos, tmp_m_zpos, m_btn;
     int m_xpos[3] = {0, 0, 0};
@@ -52,19 +69,29 @@ int main(int argc, char **argv) {
     draw_background();
 
     while (1) {
-        // マウス描画
+        // マウスイベント
         if (mouse_read(&tmp_m_xpos, &tmp_m_ypos, &tmp_m_zpos, &m_btn)) {
             queue3_push(m_xpos, tmp_m_xpos);
             queue3_push(m_ypos, tmp_m_ypos);
             queue3_push(m_zpos, tmp_m_zpos);
-            draw_mouse(m_xpos, m_ypos, m_zpos, m_btn);
+            draw(w_list, m_xpos, m_ypos, m_zpos, m_btn);
         }
 
         // キー入力
-        if (uart_inputc(0) != 0) {
-            break;
+        char key = uart_inputc(0);
+        switch (key) {
+            case 0:
+                break;
+
+            case 'w':
+                window_new(w_list, 0, 0, 100, 100);
+                break;
+
+            default:
+                goto __exit;
         }
     }
 
+__exit:
     return 0;
 }
